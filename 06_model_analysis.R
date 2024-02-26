e72666211b586b1d50c9e6c4f89c879fcb5c6b07
Meera Trivedi <meeratrivedi2026@u.northwestern.edu>
2024-02-26 13:20:05 -0600

    forgot to commit this with the past commit

--- 06_model_analysis.R
+++ 06_model_analysis.R
@@ -22,7 +22,6 @@ load(here("results/main_recipe.rda"))
 
 #load fits
 load(here("results/null_fit.rda"))
-load(here("results/basic_fit.rda"))
 load(here("results/log_fit1.rda"))
 load(here("results/log_fit2.rda"))
 
@@ -31,16 +30,11 @@ null_metrics <- null_fit |>
   collect_metrics() |> 
   mutate(model = "null")
 
-basic_metrics <- basic_fit |> 
-  collect_metrics() |> 
-  mutate(model = "baseline")
-
 log_metrics1 <- log_fit1 |> 
   collect_metrics() |> 
   mutate(model = "logistic")
 
 metrics <- null_metrics |> 
-  bind_rows(basic_metrics) |>  
   bind_rows(log_metrics1)
 
 write_csv(metrics, here("results/metrics.csv"))
@@ -51,14 +45,14 @@ metrics |>
   group_by(model) |> 
   gt() |> 
   tab_header(title = md("**Assessment Metrics**"), 
-             subtitle = "Null, Baseline, Logistic Models - Basic Recipe") |> 
+             subtitle = "Null and Logistic Models - Basic Recipe") |> 
   fmt_number(
     columns = mean, 
     decimals = 3) |> 
   fmt_number(
     columns = std_err, 
     decimals = 7) |> 
-  row_group_order(groups = c("null", "baseline", "logistic")) |> 
+  row_group_order(groups = c("null", "logistic")) |> 
   tab_options(row_group.background.color = "gray50")
 
 #main recipe assessment
